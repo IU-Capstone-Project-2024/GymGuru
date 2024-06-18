ad46a329a8523780e8f0b9a4185a8db7be25825e
Nail Minnemullin <n.minnemullin@innopolis.university>
2024-06-19 01:37:29 +0300

    Movenet pose recognition model is added

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message = ""
+
+from routes import *
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=1234, debug=True)
+
+@socketio.on("connect")
+def test_connect():
+    print("Socket successfully connected")
+    emit("my response", {"data": "Connected"})
