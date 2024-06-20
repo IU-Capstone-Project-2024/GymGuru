dd6d09f89002066ff23d40f1138cc07fbbd240c7
Nail Minnemullin <n.minnemullin@innopolis.university>
2024-06-21 00:40:39 +0300

    Login and register fixed Dockerfile added

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_login import LoginManager
-from flask_socketio import SocketIO, emit
+
+from config import Config
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "123"
-socketio = SocketIO(app)
+app.config["SECRET_KEY"] = Config.SECRET_KEY
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message = ""
 
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+
 from routes import *
 
-
-
-
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+connection = engine.connect()
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=1234, debug=True)
-
-@socketio.on("connect")
-def test_connect():
-    print("Socket successfully connected")
-    emit("my response", {"data": "Connected"})
