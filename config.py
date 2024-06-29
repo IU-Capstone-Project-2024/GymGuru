202643806173755fd18ff014d253064761bd7cd1
Nail Minnemullin <n.minnemullin@innopolis.university>
2024-06-29 16:58:03 +0300

    Docker-compose fixed

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class Config:
     # TODO: REMOVE before deployment
     DB_LOGIN = os.environ.get("DB_LOGIN") if ("DOCKER" in os.environ) else 'nai1ka'
     DB_PASSWORD = os.environ.get("DB_PASSWORD") if ("DOCKER" in os.environ) else "123456"
-
     print(DB_LOGIN, DB_PASSWORD)
     DB_NAME = "gymguru"
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_LOGIN}:{DB_PASSWORD}@{URL}:5432/{DB_NAME}"
