078427423a54d4717b6b5b223621160cb1573772
Nail Minnemullin <40440192+nai1ka@users.noreply.github.com>
2024-06-29 16:34:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = urandom(32)
     URL = 'postgres' if ("DOCKER" in os.environ) else '202.181.148.171'
     # TODO: REMOVE before deployment
-    DB_USER = os.environ.get("DB_USER") if ("DOCKER" in os.environ) else 'nai1ka'
+    DB_LOGIN = os.environ.get("DB_LOGIN") if ("DOCKER" in os.environ) else 'nai1ka'
     DB_PASSWORD = os.environ.get("DB_PASSWORD") if ("DOCKER" in os.environ) else "123456"
     DB_NAME = "gymguru"
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{URL}:5432/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_LOGIN}:{DB_PASSWORD}@{URL}:5432/{DB_NAME}"
